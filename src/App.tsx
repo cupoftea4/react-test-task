1a274e1dfb68ddd66a21622ebf025d113770ac40
Amy <nadiia.telko.pz.2021@lpnu.ua>
2023-06-07 19:40:13 +0300

    hot fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import './App.css'
-import './styles/main.scss';
 import Header from './components/Header';
 import Hero from './components/Hero';
 import Advantages from './components/Advantages';
 import About from './components/About';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import './styles/main.scss';
 
 function App() {
 
